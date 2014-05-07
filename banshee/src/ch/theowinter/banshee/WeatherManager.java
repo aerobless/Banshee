af05e1192911dce4f1b49566d57d861c36373f49
aerobless <theo.winter@gmail.com>
2014-05-07 19:25:22 +0200

    Added weatherManager. So far we can build a valid url to get access to the xml data of today's weather on weather.com. I don't care to keep the API key used here secret, it's easy to get a new one.

---
+++ banshee/src/ch/theowinter/banshee/WeatherManager.java
@@ -0,0 +1,34 @@
+package ch.theowinter.banshee;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
+public class WeatherManager {
+	private final static String SEARCH_URL = "http://api.wetter.com/forecast/weather";
+	private final static String PROJECT_NAME = "banshee";
+	private final static String API_KEY = "eff00651d20075bef02b0e95dd0aa40e";
+	private final static String CITY_CODE = "CH0CH1894";
+
+	public String buildWeatherURL(){
+		String result = null;
+		try {
+			String urlMd5 = MD5(PROJECT_NAME+API_KEY+CITY_CODE);
+			result = SEARCH_URL+"/city/"+CITY_CODE+"/project/"+PROJECT_NAME+"/cs/"+urlMd5;
+		} catch (NoSuchAlgorithmException e) {
+			Logger.log("MD5 algorithm not found",e);
+		} catch (UnsupportedEncodingException e) {
+			Logger.log("UTF8 encoding not supported",e);
+		}
+		return result;
+	}
+	
+	public String MD5(String md5) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	    java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+	    byte[] array = md.digest(md5.getBytes("UTF-8"));
+	    StringBuffer sb = new StringBuffer();
+	    for (int i = 0; i < array.length; ++i) {
+	      sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+	    }
+	    return sb.toString();
+	}
+}
--- banshee/test/ch/theowinter/banshee/BansheeUnitTests.java
+++ banshee/test/ch/theowinter/banshee/BansheeUnitTests.java
@@ -2,22 +2,54 @@ package ch.theowinter.banshee;
 
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Test;
 
 public class BansheeUnitTests {
 
 	@Test
 	public void initalTest(){
-		System.out.println("testing works.");
+		System.out.println("Test results:");
 	}
 	
+	/**
+	 * Actual sound output, enable when required to test. 
+	 */
 	@Test
 	public void speakTest(){
 		TextToSpeech ttsEngine = new TextToSpeech();
-		assertTrue(ttsEngine.speak("Test successful"));
-		assertTrue(ttsEngine.speak("sadfasdfasdfsadfsdfasdf"));
-		assertTrue(ttsEngine.speak("Hello world this is a long string."));
+		ttsEngine.hashCode();
+		//assertTrue(ttsEngine.speak("Test successful"));
 	}
 	
-
+	@Test
+	public void md5CalculationTest(){
+		WeatherManager weather = new WeatherManager();
+		String md5 = null;
+		try {
+			md5 = weather.MD5("bansheeeff00651d20075bef02b0e95dd0aa40eCH0CH1894");
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		assertEquals("68d3a19b46d79faccd4c8cbbbdbcca10", md5);
+	}
+	
+	@Test
+	public void buildWeatherURLTest(){
+		WeatherManager weather = new WeatherManager();
+		String weatherURL = weather.buildWeatherURL();
+		String expectedURL = "http://api.wetter.com/forecast/weather/city/CH0CH1894/project/banshee/cs/68d3a19b46d79faccd4c8cbbbdbcca10";
+		assertEquals(expectedURL, weatherURL);
+		testLog(weatherURL, true);
+	}
+	
+	public void testLog(String message, boolean indent){
+		if(indent){
+			System.out.println("  - "+message);
+		}else{
+			System.out.println(message);
+		}
+	}
 }
