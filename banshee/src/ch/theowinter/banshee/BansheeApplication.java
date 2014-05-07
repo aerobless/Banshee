c1bed7f563e1a75160ed4612db3c67a619a9ecf0
aerobless <theo.winter@gmail.com>
2014-05-07 17:25:02 +0200

    project setup

---
+++ banshee/src/ch/theowinter/banshee/BansheeApplication.java
@@ -0,0 +1,10 @@
+package ch.theowinter.banshee;
+
+public class BansheeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application works");
+
+	}
+
+}
---
+++ banshee/test/ch/theowinter/banshee/BansheeUnitTests.java
@@ -0,0 +1,12 @@
+package ch.theowinter.banshee;
+
+import org.junit.Test;
+
+public class BansheeUnitTests {
+
+	@Test
+	public void initalTest(){
+		System.out.println("testing works.");
+	}
+
+}
