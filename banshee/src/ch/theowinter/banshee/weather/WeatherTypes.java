dce294efc6475617341fb2190232d63f6a645444
TheoWinter <theo.winter@gmail.com>
2014-05-09 12:18:56 +0200

    added weatherType map

---
+++ banshee/src/ch/theowinter/banshee/weather/WeatherTypes.java
@@ -0,0 +1,59 @@
+package ch.theowinter.banshee.weather;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeatherTypes {
+	public static final Map<Integer, String> weatherTypes = buildWeatherList();
+	
+	private static final Map<Integer, String> buildWeatherList(){
+		Map<Integer, String> weatherTypeMap	= new HashMap<Integer, String>();
+		weatherTypeMap.put(0, "sunny");
+		weatherTypeMap.put(1, "slightly cloudy");
+		weatherTypeMap.put(2, "cloudy");
+		weatherTypeMap.put(3, "overcast");
+		weatherTypeMap.put(4, "fog");
+		weatherTypeMap.put(5, "drizzel rain");
+		weatherTypeMap.put(6, "rain");
+		weatherTypeMap.put(7, "snow");
+		weatherTypeMap.put(8, "shower");
+		weatherTypeMap.put(9, "storm");
+		weatherTypeMap.put(10, weatherTypeMap.get(1));
+		weatherTypeMap.put(20, weatherTypeMap.get(2));
+		weatherTypeMap.put(30, weatherTypeMap.get(3));
+		weatherTypeMap.put(40, weatherTypeMap.get(4));
+		weatherTypeMap.put(45, weatherTypeMap.get(4));
+		weatherTypeMap.put(48, "fog with rime formation");
+		weatherTypeMap.put(49, weatherTypeMap.get(48));
+		weatherTypeMap.put(50, weatherTypeMap.get(5));
+		weatherTypeMap.put(51, "slight drizzel rain");
+		weatherTypeMap.put(53, weatherTypeMap.get(5));
+		weatherTypeMap.put(55, "strong drizzel rain");
+		weatherTypeMap.put(56, "slight drizzel rain, freezing");
+		weatherTypeMap.put(57, "strong drizzel rain, freezing");
+		weatherTypeMap.put(60, "light rain");
+		weatherTypeMap.put(61, weatherTypeMap.get(60));
+		weatherTypeMap.put(63, "moderate rain");
+		weatherTypeMap.put(65, "strong rain");
+		weatherTypeMap.put(66, "light rain, freezing");
+		weatherTypeMap.put(67, "moderate or strong rain, freezing");
+		weatherTypeMap.put(68, "light snow-rain");
+		weatherTypeMap.put(69, "strong snow-rain");
+		weatherTypeMap.put(70, "light snowfall");
+		weatherTypeMap.put(71, weatherTypeMap.get(70));
+		weatherTypeMap.put(73, "moderate snowfall");
+		weatherTypeMap.put(75, "strong snowfall");
+		weatherTypeMap.put(80, "light rain - shower");
+		weatherTypeMap.put(81, "rain - shower");
+		weatherTypeMap.put(82, "strong rain - shower");
+		weatherTypeMap.put(83, "light snowfall - rain shower");
+		weatherTypeMap.put(84, "strong snowfall - rain shower");
+		weatherTypeMap.put(85, "light snow - shower");
+		weatherTypeMap.put(86, "moderate oder strong snowfall - shower");
+		weatherTypeMap.put(90, "storm");
+		weatherTypeMap.put(95, "light storm");
+		weatherTypeMap.put(96, "strong storm");
+		weatherTypeMap.put(999, "no data");
+		return weatherTypeMap;
+	}
+}
